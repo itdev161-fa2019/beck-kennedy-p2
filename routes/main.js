574d5c8351143573a4daf55a2806d623b9574310
beck <kennedr6@gmatc.matc.edu>
2019-12-13 03:56:37 -0600

    added partials, landing.json

--- routes/main.js
+++ routes/main.js
@@ -3,17 +3,9 @@ const router = vertex.router()
 //req localhost:3000
 //send appropros response to client
 router.get('/', (req, res) => {
-    const data = {
-        image_profile: 'http://123emoji.com/wp-content/uploads/2016/04/45.png',
-        greeting: 'hello! Welcome to my portfolio site!',
-        introduction: 'I am a web developer from Wisconsin.',
-        languages: [
-            {name:'javascript', years:2},
-            {name:'java', years:1},
-            {name:'c#', years:1},
-            {name: 'HTML/CSS', years:2}
-        ]
-    }
+    const data = req.context //{ page:..., global:...}
+    //moved it to landing.json
+    //clear separation of concerns
 
     res.render('landing', data)
 })
