74442e338b5066581f6a6b8777df5359be5b01e2
beck <kennedr6@gmatc.matc.edu>
2019-12-13 22:05:50 -0600

    added gallery, added project JSON

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,23 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const ProjectController = require('../controllers/ProjectController')
+
 //req localhost:3000
 //send appropros response to client
 router.get('/', (req, res) => {
+    //instantiation
+    const projectCtr = new ProjectController();
+    projectCtr.get()
+        .then(projects => {
+            console.log('Projects: ' + JSON.stringify(projects))
+            res.render('landing', data)
+        })
+        .catch(err => {
+            res.send('oops! ' + err.message)
+        })
+    
+
+
     const data = req.context //{ page:..., global:...}
     //moved it to landing.json
     //clear separation of concerns
